b8ed8864c6cb5b0fd74329362667425e995a7ae7
OMartsin <oleksandrmartsin@gmail.com>
2024-02-06 13:01:59 +0200

    init

---
+++ src/main/java/trandafyl/dev/hackatontest/HackatonTestApplication.java
@@ -0,0 +1,13 @@
+package trandafyl.dev.hackatontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackatonTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/trandafyl/dev/hackatontest/HackatonTestApplicationTests.java
@@ -0,0 +1,13 @@
+package trandafyl.dev.hackatontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
