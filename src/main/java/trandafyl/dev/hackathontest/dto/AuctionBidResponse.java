f072fc61c9a06702bc862779d7068bb57879a90e
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-02-07 23:16:39 +0200

    [feature] auction bid dto objects

---
+++ src/main/java/trandafyl/dev/hackathontest/dto/AuctionBidResponse.java
@@ -0,0 +1,16 @@
+package trandafyl.dev.hackathontest.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import trandafyl.dev.hackathontest.models.User;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class AuctionBidResponse {
+    private Long id;
+    private Double price;
+    private User user;
+    private LocalDateTime bidAt;
+}
