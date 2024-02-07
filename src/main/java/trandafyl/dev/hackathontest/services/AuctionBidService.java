86ec33eb515ae3a8045e9695f1f5757ece07da6a
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-02-07 23:17:03 +0200

    [feature] auction bid service

---
+++ src/main/java/trandafyl/dev/hackathontest/services/AuctionBidService.java
@@ -0,0 +1,106 @@
+package trandafyl.dev.hackathontest.services;
+
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import trandafyl.dev.hackathontest.config.BidPlacingException;
+import trandafyl.dev.hackathontest.dto.AuctionBidRequest;
+import trandafyl.dev.hackathontest.dto.AuctionBidResponse;
+import trandafyl.dev.hackathontest.dto.AuctionLotResponse;
+import trandafyl.dev.hackathontest.models.AuctionBid;
+import trandafyl.dev.hackathontest.repositories.AuctionBidRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+@Transactional
+public class AuctionBidService {
+    private final AuctionBidRepository auctionBidRepository;
+    private final AuctionLotService auctionLotService;
+    private final UserService userService;
+
+    public List<AuctionBidResponse> getBids(long lot_id) {
+        var bids = auctionBidRepository.findByAuctionLotId(lot_id);
+
+        return bids.stream().map(this::mapToDTO).toList();
+    }
+
+    public Optional<AuctionBidResponse> addBid(long lot_id, AuctionBidRequest newBid) {
+        var optionalLot = auctionLotService.getAuction(lot_id);
+        if(optionalLot.isPresent()) {
+            var lot = optionalLot.get();
+            var bid = mapFromDTO(lot, newBid);
+            if(!isValidBid(lot, bid)){
+                throw new BidPlacingException("The bid is not valid!");
+            }
+            var result = auctionBidRepository.save(bid);
+            return Optional.of(mapToDTO(result));
+        }
+        return Optional.empty();
+    }
+
+    public Optional<AuctionBidResponse> editBid(long lot_id, long bid_id, AuctionBidRequest newBidRequest){
+        var optionalLot = auctionLotService.getAuction(lot_id);
+
+        if(optionalLot.isEmpty()) {
+            return Optional.empty();
+        }
+        var lot = optionalLot.get();
+
+        var optionalBid = auctionBidRepository.findById(bid_id);
+        if(optionalBid.isEmpty()) {
+            return Optional.empty();
+        }
+        var bid = optionalBid.get();
+
+        if(!isValidBid(lot, bid)) {
+            throw new BidPlacingException("The bid is not valid!");
+        }
+
+        var newBid = mapFromDTO(lot, newBidRequest);
+        newBid.setId(bid.getId());
+
+        var result = auctionBidRepository.save(bid);
+        return Optional.of(mapToDTO(result));
+    }
+
+    public Optional<AuctionBidResponse> getBid(long bidId) {
+        var optionalBid = auctionBidRepository.findById(bidId);
+
+        return optionalBid.map(this::mapToDTO);
+    }
+
+    public void deleteBid(long bidId) {
+        auctionBidRepository.deleteById(bidId);
+    }
+
+    private boolean isValidBid(AuctionLotResponse lot, AuctionBid bid){
+        return LocalDateTime.now().isBefore(lot.getEndDateTime()) &&
+                !lot.getCreator().equals(bid.getUser()) &&
+                ((lot.getCurrentBid() != null && bid.getPrice() - lot.getCurrentBid().getPrice() >= lot.getMinIncrease()) ||
+                (lot.getCurrentBid() == null && bid.getPrice() >= lot.getStartPrice()));
+    }
+
+    public AuctionBid mapFromDTO(AuctionLotResponse lot, AuctionBidRequest bid) {
+        return AuctionBid
+                .builder()
+                .bidAt(LocalDateTime.now())
+                .auctionLot(auctionLotService.mapFromResponseDTO(lot))
+                .price(bid.getPrice())
+                .user(userService.getUser(bid.getUserId()).orElseThrow())
+                .build();
+    }
+
+    public AuctionBidResponse mapToDTO(AuctionBid bid) {
+        return AuctionBidResponse
+                .builder()
+                .id(bid.getId())
+                .bidAt(bid.getBidAt())
+                .price(bid.getPrice())
+                .user(bid.getUser())
+                .build();
+    }
+}
