60e3be7b06454c136ef2ca4a6d0891130167e6ef
OMartsin <oleksandrmartsin@gmail.com>
2024-02-07 20:46:32 +0200

    [fix] \n github standard

--- src/main/java/trandafyl/dev/hackathontest/models/ErrorMessage.java
+++ src/main/java/trandafyl/dev/hackathontest/models/ErrorMessage.java
@@ -1,9 +1,6 @@
 package trandafyl.dev.hackathontest.models;
 
-
-
 public record ErrorMessage(String message) {
-
     public static ErrorMessage from(final String message) {
         return new ErrorMessage(message);
     }
