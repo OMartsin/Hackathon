7ca0dfa897b7ff90e5124917c62e8212c589ea76
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-02-09 04:17:30 +0200

    [feature] new mapping

--- src/main/java/trandafyl/dev/hackathontest/mappers/UserMapper.java
+++ src/main/java/trandafyl/dev/hackathontest/mappers/UserMapper.java
@@ -1,14 +1,26 @@
 package trandafyl.dev.hackathontest.mappers;
 
 import org.mapstruct.Mapper;
-import trandafyl.dev.hackathontest.dto.UserPartialResponse;
+import trandafyl.dev.hackathontest.dto.UserListResponse;
 import trandafyl.dev.hackathontest.dto.UserResponse;
 import trandafyl.dev.hackathontest.models.User;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-    User toUser(UserPartialResponse userPartial);
+    User toUser(UserListResponse.PartialUserResponse userPartial);
     User toUser(UserResponse userPartial);
-    UserPartialResponse toUserPartialResponse(User user);
+    UserListResponse.PartialUserResponse toUserPartialResponse(User user);
     UserResponse toUserResponse(User user);
+
+    default UserListResponse toUserListResponse(List<User> users){
+        var partialsUsers = users.stream().map(this::toUserPartialResponse).toList();
+
+        return UserListResponse
+                .builder()
+                .response(partialsUsers)
+                .recordsCount(partialsUsers.size())
+                .build();
+    }
 }
