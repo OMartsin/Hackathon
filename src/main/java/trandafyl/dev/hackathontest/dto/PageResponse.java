eef3bcf7f0a913d622f0454d002e155f01f54b6b
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-02-09 04:08:40 +0200

    [feature] generic paginated response created

---
+++ src/main/java/trandafyl/dev/hackathontest/dto/PageResponse.java
@@ -0,0 +1,9 @@
+package trandafyl.dev.hackathontest.dto;
+
+import org.springframework.data.util.Streamable;
+
+public record PageResponse<T extends Streamable<?>>(T response, long pageRecordCount, long recordCount){
+    public PageResponse(T response, long recordCount) {
+        this(response, response.stream().count(), recordCount);
+    }
+}
